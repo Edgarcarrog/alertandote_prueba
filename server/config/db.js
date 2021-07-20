b2baaaa0e245d365516f70e6e13b8147792e09bd
Edgarcarrog <edgc21@hotmail.com>
2021-07-19 22:49:44 -0500

    database mongoAtlas config

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const conectarDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_MONGO, {
+    await mongoose.connect(process.env.DB_MONGO_ATLAS, {
       useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,
